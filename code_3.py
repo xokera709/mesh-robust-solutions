c6fc68bfd7f6ab15b872a735f7f73c7ebf8446f9
xokera709 <148460685+xokera709@users.noreply.github.com>
2023-11-22 18:44:44 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,40 @@
+    data = generate_random_data()
+def generate_random_data():
+    main()
+    for item in data:
+if __name__ == "__main__":
+
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+        print(f"Random Number: {item}")
+    return data
+def main():if __name__ == "__main__":
+import random
+    main()
+def main():
+
+        print(f"Random Number: {item}")
+    for item in data:
+
+def generate_random_data():
+
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return datadef generate_random_data():
+    for item in data:
+    main()
+
+    return data
+import random
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+    data = generate_random_data()
